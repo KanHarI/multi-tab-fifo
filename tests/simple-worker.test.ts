3932608b0f3788b17cf90b8cb8008b5e4ae5b1f6
Itay Knaan Harpaz <knaan.harpaz@gmail.com>
2021-05-11 07:40:34 +0300

    Add more tests

--- tests/simple-worker.test.ts
+++ tests/simple-worker.test.ts
@@ -17,15 +17,32 @@ test("Test worker creation long", async () => {
 });
 
 test("Test simple worker", async () => {
-  const message = 1;
+  const messages = [1, 2, 3];
   const finished_workers: Array<number> = [];
   const worker = new Worker<number>("worker_test", async (n: number) => {
     finished_workers.push(n);
   });
   await sleep(1500);
-  worker.push_message(message);
-  await sleep(150);
-  console.log(finished_workers);
-  expect(finished_workers).toStrictEqual([1]);
+  for (const message of messages) {
+    worker.push_message(message);
+  }
+  await sleep(250);
   await worker.stop();
+  expect(finished_workers).toStrictEqual([1, 2, 3]);
 });
+
+test("Test simple parallel workers", async () => {
+  const finished_workers: Array<number> = [];
+  const worker = new Worker<number>("worker_test", async (n: number) => {
+    await sleep(1000);
+    finished_workers.push(n);
+  });
+  worker.set_max_concurrent_workers(20);
+  await sleep(1500);
+  for (let i = 0; i < 100; ++i) {
+    worker.push_message(i);
+  }
+  await sleep(5500);
+  await worker.stop();
+  expect(finished_workers.length).toBe(100);
+}, 15000);
